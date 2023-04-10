894530b7e51e25ab7a1e835cffa3fab1362e5faf
王喻 <wangyu@ewell.cc>
2023-04-10 12:36:22 +0800

    SAVE

--- scripts/get_data_modify.py
+++ scripts/get_data_modify.py
@@ -1,12 +1,12 @@
-from qlib.tests.data import GetData
 import qlib
-
 # region in [REG_CN, REG_US]
-# from qlib.constant import REG_CN
+from qlib.constant import REG_CN
+
 
 provider_uri = "../qlib_data/cn_data"  # target_dir
 qlib.init(provider_uri=provider_uri, region=REG_CN)
 
 # 下载数据
+from qlib.tests.data import GetData
 aa = GetData()
-aa.qlib_data(target_dir=provider_uri, region="cn")
+aa.qlib_data(target_dir = provider_uri, region="cn")
